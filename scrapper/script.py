627287a734a4c55fbf20d8abd3c20e13d8ad67a4
Oleg <dk.sky.angel@mail.ru>
2018-12-26 15:38:32 +0200

    Add saving to files

--- scrapper/script.py
+++ scrapper/script.py
@@ -2,56 +2,29 @@ import aiohttp
 import asyncio
 import async_timeout
 from bs4 import BeautifulSoup
-from collections import Counter
+import csv
+import json
 import re
 from urllib.parse import urlparse
 import time
-
-
-async def process_string(string):
-    string = string.replace("\n", " ")
-    string = string.replace("—", "")
-    string = string.replace("|", "")
-    string = string.replace("\n", " ")
-    string = string.replace("\"", "")
-    string = string.replace("\xa0", " ")
-    string = string.replace("“", "")
-    string = string.replace("”", "")
-    string = string.replace("–", "")
-    string = string.replace(".", "")
-    string = string.replace("?", "")
-    string = string.replace(",", "")
-    string = string.replace("'ve", " have")
-    #string = string.replace(".", " || PERIOD ||")
-    #string = string.replace("?", " || QUESTION_MARK ||")
-    return string.strip()
 
 
 class Scraper:
     url: str
     root_domain: str
     domain: str
-    content_area_class: str
     search_pattern: str
 
-    def __init__(self, url: str, content_area_class: str, search_pattern: str):
+    def __init__(self, url: str, search_pattern: str):
         self.url = url
         parsed_url = urlparse(url)
         self.root_domain = parsed_url.netloc
         self.domain = parsed_url.geturl()
-        self.content_area_class = content_area_class
         self.search_pattern = search_pattern
 
     @staticmethod
     async def soup_d(html):
         return BeautifulSoup(html, 'html.parser')
-
-    async def match_path(self, path):
-        pattern = re.compile(self.search_pattern)
-        matching = pattern.match(path)
-        if  matching is None:
-            return False, None
-        return True, matching
 
     async def fetch(self, session, url: str=''):
         url = url if url else self.url
@@ -59,10 +32,12 @@ class Scraper:
             async with session.get(url) as response:
                 return await response.text()
 
-    async def extract_content(self, html):
-        soup = await self.soup_d(html)
-        content_area = soup.find("div", {"class": self.content_area_class})
-        return content_area.text
+    async def match_path(self, path):
+        pattern = re.compile(self.search_pattern)
+        matching = pattern.match(path)
+        if  matching is None:
+            return False, None
+        return True, matching
 
     async def extract_links(self, html, local_only=True):
         soup = await self.soup_d(html)
@@ -79,28 +54,65 @@ class Scraper:
                         paths.append(f'{self.domain}{path}')
         return list(set(paths))
 
+    # methods to override
+    async def extract_content(self, html):
+        soup = await self.soup_d(html)
+        content_area = soup.find("div", {"class": "entry-content"})
+        return content_area.text
+
+    async def extract_title(self, html):
+        soup = await self.soup_d(html)
+        content_area = soup.find("h1", {"class": "entry-title"})
+        return content_area.text
+
+    async def write_to(self, extension: str, data: list):
+        if not data:
+            return
+        if extension == 'json':
+            with open(f'data.{extension}', 'w') as outfile:
+                json.dump(
+                    data, 
+                    outfile, 
+                    sort_keys=True, 
+                    indent=4, 
+                    ensure_ascii=False
+                )
+        if extension == 'csv':
+            with open(f'data.{extension}', 'w') as outfile:
+                fieldnames = data[0].keys()
+                writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+
+                writer.writeheader()
+                for item in data:
+                    writer.writerow(item)
+
     async def run(self):
         async with aiohttp.ClientSession() as session:
             start_time = time.time()
             html = await self.fetch(session)
             paths = await self.extract_links(html)
-            words = []
+            data = []
             for path in paths:
-                print('post_url:', path)
+                print(path)
                 post_html = await self.fetch(session, path)
                 post_content = await self.extract_content(post_html)
-                string = await process_string(post_content)
-                words += string.split()
-            word_freq = Counter(words)
+                post_title = await self.extract_title(post_html)
+                data.append(
+                    {
+                        'url': path,
+                        'title': post_title,
+                        'content': post_content[:500]
+                    }
+                )
+            await self.write_to('json', data)
+            await self.write_to('csv', data)
             print("Entire request took", time.time()-start_time, "seconds")
-            print(word_freq.most_common(10))
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(
         Scraper(
             url='http://tim.blog',
-            content_area_class='entry-content',
             search_pattern=r'^/(?P<year>\d+){4}/(?P<month>\d+){2}/(?P<day>\d+){2}/(?P<slug>[\w-]+)/$'
         ).run()
     )
